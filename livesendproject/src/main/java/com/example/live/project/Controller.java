8a12eb8e6ef1450d1a5828aaf13e2562c2c21838
santhoshkumarprabakaran <santhoshsanty9080@gmail.com>
2023-05-04 22:43:34 +0530

    requed commit

--- livesendproject/src/main/java/com/example/live/project/Controller.java
+++ livesendproject/src/main/java/com/example/live/project/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-	@RequestMapping("/home")
+	@RequestMapping("/")
 	public String main() {
 		return "helloworldfrom santhosh";
 	}
