07767844ad07842690b01b825a3e09912c8cc558
santhoshkumarlabs <santhoshsanty9080@gmail.com>
2023-05-04 22:20:49 +0530

    first commit

---
+++ livesendproject/src/main/java/com/example/live/project/LiveProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.live.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveProjectApplication.class, args);
+	}
+
+}
